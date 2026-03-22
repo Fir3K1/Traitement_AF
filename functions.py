765de5ead14ec1eb8febd59c58b29a7a1a13b573
Florent <florent0614@gmail.com>
2026-03-22 13:14:19 +0100

    ajout de commentaires

--- functions.py
+++ functions.py
@@ -12,23 +12,24 @@ def lecture_automate(chemin):
     with open(chemin, 'r') as AF:
         donnees = [ligne.strip() for ligne in AF.readlines()]
 
-    nb_transitions = int(donnees[4])
+    nb_transitions = int(donnees[4]) # int : nombre de transitions
 
-    etats_initiaux = donnees[2].split()[1:]
-    etats_finaux = donnees[3].split()[1:]
+    etats_initiaux = donnees[2].split()[1:] # char : récupère l'état initial
+    etats_finaux = donnees[3].split()[1:] # char : récupère l'état final
 
-    transitions = []
-    lettres = []
-    etats = []
+    transitions = [] # liste dans laquel il y aura des tuples de trois éléments (départ, lettre, arrivée) représentant une transition
+    lettres = [] # liste des lettres 
+    etats = [] #liste des états
 
     for donnee in donnees[5:5+nb_transitions]:
-        depart, lettre, arrivee = donnee.split()
+        depart, lettre, arrivee = donnee.split() #récupère les éléments (char) qui son séparés d'un espace
 
-        transitions.append((depart, lettre, arrivee))
+        transitions.append((depart, lettre, arrivee)) #insère le tuple dans la liste des transitions
 
+        #insère la lettre dans la liste si elle n'y est pas encore 
         if lettre not in lettres:
                 lettres.append(lettre)
-        lettres.sort()
+        lettres.sort() #range dans l'ordre
 
         if depart not in etats:
             etats.append(depart)

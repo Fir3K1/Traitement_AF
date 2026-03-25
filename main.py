6fd2fbbac77217857323d77842bd03bd91122c86
Florent <florent0614@gmail.com>
2026-03-25 23:42:38 +0100

    interface utilisateur v1

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from functions import *
 
 def main():
-    print("hello")
-    #print(affichage("AF1.txt"))
-    
+    print("Quel automate souhaitez-vous tester ?")
+
+    while True:
+        try:
+            AF = int(input("Saisissez un numéro entre 1 et 44 : "))
+            if 1 <= AF <= 44:
+                break
+            else:
+                print("\nValeur incorrecte. ")
+        except ValueError:
+            print("\nValeur incorrecte. ")
+
+    chemin = f"AF/AF{str(AF)}.txt"
+
+    automate = lecture_automate(chemin)
+    print(automate)
+
+    print(Affichage(automate.alphabet, automate.etats, automate.initial, automate.final, automate.transitions))
+
+    print("Fin")
     
 main()
